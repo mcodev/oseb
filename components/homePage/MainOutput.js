80a1130b8218b245ddca5d68fe2bccb4f02e7716
mcodev <mcodevns@gmail.com>
2021-08-10 11:57:05 +0300

    new

--- components/homePage/MainOutput.js
+++ components/homePage/MainOutput.js
@@ -6,24 +6,57 @@ import { width } from "../../constants/device";
 export default function MainOutput() {
   return (
     <View style={styles.container}>
-      <View style={styles.cirleOne}>
-        <Text>test1</Text>
-      </View>
-      <View style={styles.circleTwo}>
-        <Text style={{ color: colors.white }}>test1</Text>
+      <View style={styles.textContainer}>
+        <View style={styles.prevContainer}>
+          <Text>test1</Text>
+        </View>
+        <View style={styles.nextContainer}>
+          <Text>test1</Text>
+        </View>
       </View>
     </View>
+    // <View style={styles.container}>
+    //   <View style={styles.cirleOne}>
+    //     <Text>test1</Text>
+    //   </View>
+    //   <View style={styles.circleTwo}>
+    //     <Text style={{ color: colors.white }}>test1</Text>
+    //   </View>
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 30,
+    // backgroundColor: "aqua",
     flex: 8,
-    flexDirection: "row",
-    alignItems: "center",
     paddingHorizontal: 5,
+    alignItems: "center",
     justifyContent: "center",
   },
+  textContainer: {
+    flexDirection: "row",
+    height: "40%",
+  },
+  prevContainer: {
+    flex: 1,
+    // backgroundColor: "aqua",
+    height: "100%",
+    borderRightWidth: 1,
+    borderRightColor: colors.black,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  nextContainer: {
+    // backgroundColor: "yellow",
+    flex: 1,
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   cirleOne: {
     backgroundColor: colors.white,
     height: width * 0.5,
